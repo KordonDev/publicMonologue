64a1145b28464468c250703a184bde9d8c52bb98
Arne Maier <Arne_Maier@gmx.de>
2015-02-24 16:39:59 +0100

    Spring Security with testuser.

--- publicMonologue/grails-app/conf/BootStrap.groovy
+++ publicMonologue/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import publicmonologue.Role
+import publicmonologue.User
+import publicmonologue.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def author = new Role(authority: 'ROLE_AUTHOR').save(flush: true)
+        def testUser = new User(username: 'e', password: 'e').save(flush: true)
+
+        UserRole.create(testUser, author, true)
+
+        assert User.count == 1
+        assert Role.count == 1
+        assert UserRole.count == 1
     }
     def destroy = {
     }
